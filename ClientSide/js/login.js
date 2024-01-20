9288acedd63326f45115fead5a599d6565f4ed88
happy-kitty0821 <ayushhaang09@gmail.com>
2024-01-20 16:43:09 +0545

    clinet side init

--- ClientSide/js/login.js
+++ ClientSide/js/login.js
@@ -1,3 +1,7 @@
-function login(){
-  
+function showmessage(){
+    alert("hahahaha!! in your favce boey")
+}
+
+function info(){
+    alert('maybe this is not the actual login page ^_^; maybe there is another secret page!! who knows')
 }
---
+++ ClientSide/js/secret.js
@@ -0,0 +1,13 @@
+//how many encryptions are there??????
+//there is a saying that only the username must match to be login
+//base 64 = R0kzREdSUlRHUVpUSU0yRUdJM0RHUlJVR0UyRFNOQlhHTVpESU5JPQ==
+
+const username = "R0kzREdSUlRHUVpUSU0yRUdJM0RHUlJVR0UyRFNOQlhHTVpESU5JPQ==s"
+
+function msg1(){
+    alert("yooo mate chill y u tryin to change password of the account that u dont even own ^_^ bruhhhh")
+}
+
+function msg2(){
+    alert("This system doesnt even have a Database!!! How are u goin to create a account")
+}
